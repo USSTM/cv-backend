72538bfdf3b495adeb4426db443626eb5fc1dfd8
zazu7765 <ohmyzazu@gmail.com>
2025-01-23 14:27:18 -0500

    Sample server test

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/USSTM/cv-backend/api"
+	"github.com/USSTM/cv-backend/api/oas"
+	"github.com/go-chi/chi/v5"
+)
+
 //go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=../api/config.yaml ../api/swagger.yaml
+//
+
+func main() {
+	server := api.NewServer()
+
+	r := chi.NewMux()
+
+	oas.HandlerFromMux(server, r)
+
+	s := &http.Server{
+		Handler: r,
+		Addr:    "0.0.0.0:8080",
+	}
+
+	fmt.Println("Banana")
+
+	log.Fatal(s.ListenAndServe())
+}
